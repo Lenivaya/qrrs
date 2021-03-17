4773a77b13df52451e924f0f4b4f1b35a28ac531
Lenivaya <xocada@gmail.com>
2021-03-17 15:06:12 +0200

    More obvious filename

--- src/lib.rs
+++ src/lib.rs
@@ -150,10 +150,10 @@ mod tests {
     #[test]
     fn make_code() {
         let text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
-        let file = "qr_tmp.png";
+        let file = "qr_tmp_lorem.png";
 
         let config = cli::Config {
-            input: Some(text),
+            input: Some(&text),
             output: Some(file),
             read: false,
             terminal_output: false,

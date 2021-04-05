5245da48688feed57d2597aba0a00cd82217a55f
Lenivaya <xocada@gmail.com>
2021-04-05 09:24:26 +0300

    Add tarpaulin to shell.nix

--- shell.nix
+++ shell.nix
@@ -4,7 +4,7 @@ with pkgs;
 
 mkShell {
   name = "qrrs";
-  buildInputs = [ cargo rustc glibc pkg-config rustfmt ];
+  buildInputs = [ cargo cargo-tarpaulin rustc glibc pkg-config rustfmt ];
 
   RUST_BACKTRACE = 1;
   RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";

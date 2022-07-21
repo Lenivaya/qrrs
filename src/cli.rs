5db266101e429e7d5c59cb35370fc77ee5b7b0a2
Lenivaya <xocada@gmail.com>
2022-07-22 01:41:35 +0300

    delete garbage

--- src/cli.rs
+++ src/cli.rs
@@ -6,22 +6,18 @@ pub struct Arguments {
     /// Input data
     #[clap(
         name = "INPUT",
-        // parse(from_os_str),
         value_hint = ValueHint::AnyPath,
         required(true),
         index(1)
     )]
-    // pub input: Option<PathBuf>,
     pub input: Option<String>,
 
     #[clap(
         name = "OUTPUT",
-        // parse(from_os_str),
         value_hint = ValueHint::AnyPath,
         required_unless_present_any(["INPUT", "read", "terminal"]),
         index(2)
     )]
-    // pub output: Option<PathBuf>,
     pub output: Option<String>,
 
     /// Reads the qr-code instead of generating it
@@ -31,6 +27,4 @@ pub struct Arguments {
     /// Displays code in terminal
     #[clap(name = "terminal", short = 't', long = "terminal")]
     pub terminal_output: bool,
-    // pub matches: ArgMatches,
 }
-

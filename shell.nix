db191160f03999353a9c92d75c4e2237648f7658
Lenivaya <xocada@gmail.com>
2022-07-22 01:41:52 +0300

    format shell.nix

--- shell.nix
+++ shell.nix
@@ -1,11 +1,16 @@
-{ pkgs ? import <nixpkgs> { } }:
-
+{pkgs ? import <nixpkgs> {}}:
 with pkgs;
+  mkShell {
+    name = "qrrs";
+    buildInputs = [
+      cargo
+      cargo-tarpaulin
+      # rustc
+      glibc
+      pkg-config
+      rustfmt
+    ];
 
-mkShell {
-  name = "qrrs";
-  buildInputs = [ cargo cargo-tarpaulin rustc glibc pkg-config rustfmt ];
-
-  RUST_BACKTRACE = 1;
-  RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
-}
+    RUST_BACKTRACE = 1;
+    # RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+  }

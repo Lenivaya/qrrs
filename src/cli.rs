6ddd521f1aa2cd549b8f63ecc2add045e9691bb5
Lenivaya <xocada@gmail.com>
2022-10-10 17:44:37 +0300

    add examples of usage to help message

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,27 @@
-use clap::{Parser, ValueHint};
+use clap::{ColorChoice, Parser, ValueHint};
+
+const AFTER_TEXT: &str = "
+Examples:
+  qrrs \"Some text\"
+  qrrs \"Some text\" /tmp/qr.png
+
+  qrrs --read /tmp/qr.png
+  Some text
+
+  qrrs --read /tmp/qr.png /tmp/qr1.png
+  qrrs --read /tmp/qr1.png
+  Some text
+";
 
 #[derive(Parser, Debug)]
-#[clap(name = "qrrs", author, about, version)]
+#[clap(
+    name = "qrrs",
+    author,
+    about,
+    version,
+    after_help = AFTER_TEXT,
+    color = ColorChoice::Always
+)]
 pub struct Arguments {
     /// Input data
     #[clap(

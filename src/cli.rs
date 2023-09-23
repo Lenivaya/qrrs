222b2c233060e20e9c53e504ba178f30c24fe1f9
Lenivaya <xocada@gmail.com>
2023-09-23 11:07:06 +0300

    feat: allow specifying unicode as output format

--- src/cli.rs
+++ src/cli.rs
@@ -74,6 +74,7 @@ pub enum OutputFormat {
     #[default]
     Image,
     Svg,
+    Unicode,
 }
 
 impl Arguments {
--- src/qrcode.rs
+++ src/qrcode.rs
@@ -28,7 +28,7 @@ pub fn to_svg(code: &QrCode, view_arguments: QrCodeViewArguments) -> String {
 }
 
 /// Renders the QR code into the unicode string that can be used in terminal
-pub fn to_terminal(code: &QrCode, view_arguments: QrCodeViewArguments) -> String {
+pub fn to_unicode(code: &QrCode, view_arguments: QrCodeViewArguments) -> String {
     let (dark_color, light_color) = match view_arguments.invert_colors {
         true => (unicode::Dense1x2::Dark, unicode::Dense1x2::Light),
         false => (unicode::Dense1x2::Light, unicode::Dense1x2::Dark),
@@ -54,7 +54,7 @@ pub fn to_image(code: &QrCode, view_arguments: QrCodeViewArguments) -> DynamicIm
 }
 
 pub fn print_code_to_term(code: &QrCode, view_arguments: QrCodeViewArguments) {
-    println!("\n{}", to_terminal(code, view_arguments));
+    println!("\n{}", to_unicode(code, view_arguments));
 }
 
 pub fn read_data_image(file: &Path) -> BoxResult<Vec<String>> {
@@ -78,19 +78,27 @@ pub fn read_data_image(file: &Path) -> BoxResult<Vec<String>> {
 }
 
 pub fn save(file: &Path, code: &QrCode, image_save_args: ImageSaveArguments) -> BoxResult<()> {
-    match image_save_args.output_format {
-        OutputFormat::Image => save_image(file, code, image_save_args),
-        OutputFormat::Svg => save_svg(file, code, image_save_args),
-    }
+    let save_fn: fn(&Path, &QrCode, QrCodeViewArguments) -> BoxResult<()> =
+        match image_save_args.output_format {
+            OutputFormat::Image => save_image,
+            OutputFormat::Svg => save_svg,
+            OutputFormat::Unicode => save_unicode,
+        };
+    save_fn(file, code, image_save_args.view_arguments)
 }
 
-fn save_image(file: &Path, code: &QrCode, args: ImageSaveArguments) -> BoxResult<()> {
-    let image = to_image(code, args.view_arguments);
+fn save_unicode(file: &Path, code: &QrCode, view_arguments: QrCodeViewArguments) -> BoxResult<()> {
+    let unicode_image = to_unicode(code, view_arguments);
+    fs::write(file, unicode_image).map_err(|err| handle_save_error(file, err))
+}
+
+fn save_image(file: &Path, code: &QrCode, view_arguments: QrCodeViewArguments) -> BoxResult<()> {
+    let image = to_image(code, view_arguments);
     image.save(file).map_err(|err| handle_save_error(file, err))
 }
 
-fn save_svg(file: &Path, code: &QrCode, args: ImageSaveArguments) -> BoxResult<()> {
-    let svg_image = to_svg(code, args.view_arguments);
+fn save_svg(file: &Path, code: &QrCode, view_arguments: QrCodeViewArguments) -> BoxResult<()> {
+    let svg_image = to_svg(code, view_arguments);
     fs::write(file, svg_image).map_err(|err| handle_save_error(file, err))
 }
 
